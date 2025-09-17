08a3dc7629ec6df49c245344efb0afc881fa3614
anasadnan2 <anasadnanabojaber@gmail.com>
2025-09-17 11:18:28 +0300

    Add default config (regions, prayers, endpoints)

---
+++ src/config.js
@@ -0,0 +1,20 @@
+export const REGIONS = [
+  { value: "africa", label: "أفريقيا" },
+  { value: "americas", label: "الأمريكيتان" }, // (الشمالية والجنوبية)
+  { value: "asia", label: "آسيا" },
+  { value: "europe", label: "أوروبا" },
+  { value: "oceania", label: "أوقيانوسيا" },
+];
+
+export const MAIN_PRAYERS = ["الفجر", "الظهر", "العصر", "المغرب", "العشاء"];
+
+export const ENDPOINTS = {
+  countriesByRegion: (region) =>
+    `https://restcountries.com/v3.1/region/${region}`,
+
+  citiesByCountry: "https://countriesnow.space/api/v0.1/countries/cities",
+
+  methods: "https://api.aladhan.com/v1/methods",
+
+  timingsByCity: "https://api.aladhan.com/v1/timingsByCity",
+};
